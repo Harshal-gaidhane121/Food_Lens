762c9a1bcfd180ed32a415c2e55f826049312753
Harshal-Gaidhane121 <harshalgaidhane121@gmail.com>
2025-03-10 10:57:07 +0530

    just Trying

--- app/src/main/java/com/example/foodlens/networks/LoginRetrofitClient.kt
+++ app/src/main/java/com/example/foodlens/networks/LoginRetrofitClient.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.97.153:3000/api/v1/" // Replace with your local IP
+    private const val BASE_URL = "http://192.168.97.62:3000/api/v1/" // Replace with your local IP
 
     private var token: String? = null
 
